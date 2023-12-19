2307a847b32c9124bc2ae4f7d67c1ab09ca6bebc
CedricSanchezGithub <mycedmail@gmail.com>
2023-12-19 22:08:58 +0100

    commit

--- front-end/script.js
+++ front-end/script.js
@@ -2,7 +2,8 @@
 
 const good = document.getElementById("goodAnswer");
 const bad = document.getElementById("badAnswer");
-const imgBagueton = document.getElementById('imgBagueton');
+const imgBagueton = document.getElementById("imgBagueton");
+const inputRef = document.getElementById("ref");
 // Fonction permetant d'afficher bonne réponse selon le booléen
 
 function getValue() {
@@ -11,18 +12,19 @@ function getValue() {
     console.log("bonne réponse");
     good.style.opacity = "1";
     bad.style.opacity = "0";
-
+    inputRef.style.background = "#6cba6c";
   } else {
     console.log("mauvaise réponse");
     bad.style.opacity = "1";
     good.style.opacity = "0";
+    inputRef.style.background = "#e98888";
   }
   console.log(input);
-  event.preventDefault() // A changer car déprécié
-  ;
+  event.preventDefault(); // A changer car déprécié
 }
 
-imgBagueton.addEventListener('mouseover', function() {
+imgBagueton.addEventListener("mouseover", function () {
   imgBagueton.style.opacity = "1";
+  event.preventDefault();
   console.log("mouse over!");
 });
