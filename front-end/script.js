4a293004b4d7404e2c9ecbca17e8252e11ef5dbe
CedricSanchezGitHub <mycedmail@gmail.com>
2023-12-19 18:49:14 +0100

    site avancé

--- front-end/script.js
+++ front-end/script.js
@@ -1,1 +1,23 @@
 // alert("hello, world")
+
+const good = document.getElementById("goodAnswer");
+const bad = document.getElementById("badAnswer");
+
+// Fonction permetant d'afficher bonne réponse selon le booléen
+
+function getValue() {
+  const input = document.getElementById("ref").value;
+  if (input == "marmiton") {
+    console.log("bonne réponse");
+    good.style.opacity = "1";
+    bad.style.opacity = "0";
+
+  } else {
+    console.log("mauvaise réponse");
+    bad.style.opacity = "1";
+    good.style.opacity = "0";
+  }
+  console.log(input);
+  event.preventDefault() // A changer car déprécié
+  ;
+}
